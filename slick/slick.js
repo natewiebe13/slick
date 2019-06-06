fab45893d1925bc33bf8e06d977f0eb3f76c6a49
Nate Wiebe <nate@northern.co>
2019-06-06 12:49:15 -0400

    Update center class on cloned slides to fix issues with infinity mode

--- slick/slick.js
+++ slick/slick.js
@@ -2354,7 +2354,7 @@
                 if (index === 0) {
 
                     allSlides
-                        .eq(allSlides.length - 1 - _.options.slidesToShow)
+                        .eq(allSlides.length - 1 - _.options.slidesToShow - (_.slideCount - 1 - _.options.slidesToShow))
                         .addClass('slick-center');
 
                 } else if (index === _.slideCount - 1) {
